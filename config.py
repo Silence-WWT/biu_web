c06837613ce8cdb0e380a9aa16a2a5e96c508a96
silencewwt <silencewwt@gmail.com>
2014-11-21 09:35:19 +0800

    add config, manage, database.rst

---
+++ config.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    # TODO: add a random string as the default SECRET_KEY.
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    CDN_DEBUG = False
+    PHOTO_DIR = os.path.join(basedir, 'photos')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'mysql+pymysql://dev:devpassword@localhost/biu?charset=utf8'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'mysql+pymysql://test:testpassword@localhost/biu_test?charset=utf8'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'mysql+pymysql://dbuser:usER_2014@node1.db/znx?charset=utf8'
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
