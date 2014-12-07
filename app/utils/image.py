cbe0a302981870f864752a9b71df18321ef7725d
silencewwt <silencewwt@gmail.com>
2014-12-07 12:41:06 +0800

    add get_image_from_url in utils.image

--- app/utils/image.py
+++ app/utils/image.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import requests
 from base64 import b64decode
 from cStringIO import StringIO
 from hashlib import md5
@@ -13,7 +14,7 @@ def generate_dir_path(user_id):
     return '%s/user_%s/' % (user_id % 100, md5(str(user_id)).hexdigest())
 
 
-def upload_image(user_id, image_base64_string):
+def upload_image(user_id, image_string, is_base64=True):
     path = current_app.config['IMAGE_DIR']
     relative_path = generate_dir_path(user_id)
     dir_path = os.path.join(path, relative_path)
@@ -22,8 +23,26 @@ def upload_image(user_id, image_base64_string):
     image_name = uuid4().hex
     image_path = os.path.join(dir_path, image_name)
     try:
-        image = Image.open(StringIO(b64decode(image_base64_string)))
+        if is_base64:
+            image_string = b64decode(image_string)
+        image = Image.open(StringIO(image_string))
     except:
-        return False
+        return ''
     image.save(image_path, image.format, quality=75)
     return relative_path + image_name
+
+
+def get_image_from_url(user_id, image_url):
+    if not image_url:
+        image_string = ''
+    else:
+        try:
+            resp = requests.get(image_url)
+            image_string = resp.content
+        except:
+            image_string = ''
+    if image_string:
+        image_path = upload_image(user_id, image_string, False)
+    else:
+        image_path = ''
+    return image_path
