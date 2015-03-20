5fd0a21819ef2081daf9e2505de8d4fd3ffb02a2
silencewwt <silencewwt@gmail.com>
2015-03-20 23:05:04 +0800

    tag td -> th in thead

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -11,7 +11,9 @@ admin = Blueprint('biu_admin', __name__)
 from .views import BiuBaseView
 admin_app.add_view(BiuBaseView(User, db.session, name='user', template='admin/user.html', endpoint='admin_user'))
 admin_app.add_view(BiuBaseView(Post, db.session, name='post', template='admin/post.html', endpoint='admin_post'))
-# admin_app.add_view(ModelView(User, db.session, 'ModelUser', url='/admin/User'))
+admin_app.add_view(BiuBaseView(PostComment, db.session, name='comment', template='admin/comment.html',
+                               endpoint='admin_comment'))
+admin_app.add_view(ModelView(User, db.session, 'ModelUser', url='/admin/User'))
 # admin_app.add_view(ModelView(Post, db.session, 'Post', url='/admin/post'))
 # admin_app.add_view(ModelView(PostComment, db.session, 'post_comment'))
 # admin_app.add_view(ModelView(Channel, db.session, 'channel', url='/admin/channel'))
